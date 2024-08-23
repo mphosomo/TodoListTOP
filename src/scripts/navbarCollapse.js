faa106fd8da1f53e937763503adf020afa23a14b
Fantom <officialfantomwrld@gmail.com>
2024-08-23 12:27:43 +0200

    Implement disappearing navbar toggle button on main content

--- src/scripts/navbarCollapse.js
+++ src/scripts/navbarCollapse.js
@@ -1,12 +1,18 @@
 const navbar = document.querySelector('aside');
 const navbarCollapseButtons = document.querySelectorAll('.sidebar-toggle');
+const navBarCollapseButtonFromSidebar = document.querySelector('#navbar-toggle-from-sidebar');
+const navbarCollapseButtonFromMain = document.querySelector('#navbar-toggle-from-main');
 
-function collapseSidebar() {
+const collapseSidebar = (function() {
     navbarCollapseButtons.forEach(button => {
         button.addEventListener('click', () => {
             navbar.classList.toggle('closed');
         });
     });
-}
+})();
 
-collapseSidebar();
+const displayMainPageSideBar = (function() {
+    navBarCollapseButtonFromSidebar.addEventListener('click', () => {
+        navbarCollapseButtonFromMain.style.display = 'flex';
+    })
+})();
