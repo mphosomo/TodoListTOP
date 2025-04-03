2f962e1b189e2e8608253a7483d6e58f3da97f26
programmingwithfantom <mphosomo07@gmail.com>
2025-04-03 08:15:26 +0200

    feat: implement create task modal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,37 @@
 import '../style.css';
 
-const newProjectButton = document.querySelector('#add-new-project');
 const modalContainer = document.querySelector('.modal-container');
+
+const newProjectButton = document.querySelector('#add-new-project');
+const projectCreateModal = document.querySelector('.create-project-modal');
 const closeProjectCreateModal = document.querySelector(
 	'#close-project-creation-modal'
 );
 
+const newTaskButton = document.querySelector('#add-new-task');
+const taskCreateModal = document.querySelector('.create-task-modal');
+const closeTaskCreateModal = document.querySelector(
+	'#close-task-creation-modal'
+);
+
+// New Project Modal
 newProjectButton.addEventListener('click', () => {
 	modalContainer.classList.add('active');
+	projectCreateModal.classList.add('active');
 });
 
 closeProjectCreateModal.addEventListener('click', () => {
 	modalContainer.classList.remove('active');
+	projectCreateModal.classList.remove('active');
+});
+
+// New Task Modal
+newTaskButton.addEventListener('click', () => {
+	modalContainer.classList.add('active');
+	taskCreateModal.classList.add('active');
+});
+
+closeTaskCreateModal.addEventListener('click', () => {
+	modalContainer.classList.remove('active');
+	taskCreateModal.classList.remove('active');
 });
