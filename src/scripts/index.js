f131a5fbe74c7412e91ea7cb3efc9ea60d855e74
programmingwithfantom <mphosomo07@gmail.com>
2025-04-03 07:45:14 +0200

    feat: implement create project modal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,15 @@
 import '../style.css';
+
+const newProjectButton = document.querySelector('#add-new-project');
+const modalContainer = document.querySelector('.modal-container');
+const closeProjectCreateModal = document.querySelector(
+	'#close-project-creation-modal'
+);
+
+newProjectButton.addEventListener('click', () => {
+	modalContainer.classList.add('active');
+});
+
+closeProjectCreateModal.addEventListener('click', () => {
+	modalContainer.classList.remove('active');
+});
