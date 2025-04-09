7708ed33da11c49f188ecc8d117f3d6dc346070d
programmingwithfantom <mphosomo07@gmail.com>
2025-04-09 15:40:47 +0200

    feat: add getter for formatted date

--- src/scripts/task.js
+++ src/scripts/task.js
@@ -1,11 +1,30 @@
+import { format } from 'date-fns';
+
 export default class Task {
-	constructor(name, description, priority, dueDate, isComplete) {
+	constructor(name, description, priority, dueDate) {
 		this.taskId = 'task-' + crypto.randomUUID().substring(0, 8);
 		this.name = name;
 		this.description = description;
 		this.priority = priority;
 		this.dueDate = dueDate;
-		this.isComplete = isComplete;
+		this.isComplete = false;
+	}
+
+	toggleCompletedStatus() {
+		this.isComplete = !this.isComplete;
+	}
+
+	getFormattedDate() {
+		const formattedDate = format(this.dueDate, 'do, MMM, yyyy');
+		const formattedDateArr = formattedDate.split(',');
+		const trimmedFormattedDateArr = formattedDateArr.map((element) => {
+			return element.trim();
+		});
+		const dayNumber = trimmedFormattedDateArr[0];
+		const month = trimmedFormattedDateArr[1];
+		const year = trimmedFormattedDateArr[2];
+
+		return `${dayNumber} of ${month}, ${year}`;
 	}
 
 	toString() {
@@ -16,7 +35,7 @@ export default class Task {
 export const demoTask = new Task(
 	'Demo Task',
 	'This is a demo task',
-	'high-priority',
+	'High',
 	'2025-04-02',
 	false
 );
