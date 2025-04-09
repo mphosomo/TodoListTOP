2d3a94177dde6cdda491eb97403711ec7e7bc216
programmingwithfantom <mphosomo07@gmail.com>
2025-04-09 15:41:34 +0200

    feat: add addTask to project

--- src/scripts/project.js
+++ src/scripts/project.js
@@ -13,59 +13,13 @@ export default class Project {
 		this.tasks = [];
 	}
 
+	addTask(name, description, priority, dueDate) {
+		const task = new Task(name, description, priority, dueDate);
+		this.tasks.push(task);
+	}
+
 	getTasks() {
-		this.tasks.forEach((task) => {
-			return task.toString();
-		});
-	}
-
-	createNewTask(name, description, priority, dueDate, isComplete) {
-		const newTask = new Task(
-			name,
-			description,
-			priority,
-			dueDate,
-			isComplete
-		);
-		this.tasks.push(newTask);
-
-		return `Task created with ID ${newTask.taskId}`;
-	}
-
-	editTask(
-		taskId,
-		newName,
-		newDescription,
-		newPriority,
-		newDueDate,
-		newCompleteStatus
-	) {
-		const task = this.tasks.find((task) => task.taskId === taskId);
-
-		if (task) {
-			task.name = newName;
-			task.description = newDescription;
-			task.priority = newPriority;
-			task.dueDate = newDueDate;
-			task.isComplete = newCompleteStatus;
-
-			return `Task with ID '${taskId}' edited!`;
-		} else {
-			return `Task with ID '${taskId}' does not exist!`;
-		}
-	}
-
-	deleteTask(taskId) {
-		const taskIndex = this.tasks.findIndex(
-			(task) => task.taskId === taskId
-		);
-
-		if (taskIndex !== -1) {
-			this.tasks.splice(taskIndex, 1);
-			return `Task with ID '${taskId}' deleted!`;
-		} else {
-			return `Task with ID '${taskId}' does not exist!`;
-		}
+		return this.tasks;
 	}
 
 	toString() {
@@ -78,10 +32,9 @@ export const demoProject = new Project(
 	'This is a demo project'
 );
 
-demoProject.createNewTask(
+demoProject.addTask(
 	demoTask.name,
 	demoTask.description,
 	demoTask.priority,
-	demoTask.dueDate,
-	demoTask.isComplete
+	demoTask.dueDate
 );
