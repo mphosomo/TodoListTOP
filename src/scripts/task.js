344b0559b594cab159a8edd81ed61ceb27bd93aa
programmingwithfantom <mphosomo07@gmail.com>
2025-04-21 12:14:22 +0200

    fix: task structure

--- src/scripts/task.js
+++ src/scripts/task.js
@@ -1,7 +1,7 @@
-import { format } from 'date-fns';
+import { format } from "date-fns"
 
 function generateUUID() {
-	return 'task-' + crypto.randomUUID().substring(0, 8);
+	return "task-" + crypto.randomUUID().substring(0, 8)
 }
 
 export default class Task {
@@ -13,28 +13,28 @@ export default class Task {
 		taskId = null,
 		isComplete = false
 	) {
-		this.taskId = taskId || generateUUID();
-		this.name = name;
-		this.description = description;
-		this.priority = priority;
-		this.dueDate = dueDate;
-		this.isComplete = isComplete;
+		this.taskId = taskId || generateUUID()
+		this.name = name
+		this.description = description
+		this.priority = priority
+		this.dueDate = dueDate
+		this.isComplete = isComplete
 	}
 
 	toggleCompletedStatus() {
-		this.isComplete = !this.isComplete;
+		this.isComplete = !this.isComplete
 	}
 
 	getFormattedDate() {
-		const formattedDate = format(this.dueDate, 'do, MMM, yyyy');
-		const formattedDateArr = formattedDate.split(',');
+		const formattedDate = format(this.dueDate, "do, MMM, yyyy")
+		const formattedDateArr = formattedDate.split(",")
 		const trimmedFormattedDateArr = formattedDateArr.map((element) => {
-			return element.trim();
-		});
-		const dayNumber = trimmedFormattedDateArr[0];
-		const month = trimmedFormattedDateArr[1];
-		const year = trimmedFormattedDateArr[2];
+			return element.trim()
+		})
+		const dayNumber = trimmedFormattedDateArr[0]
+		const month = trimmedFormattedDateArr[1]
+		const year = trimmedFormattedDateArr[2]
 
-		return `${dayNumber} of ${month}, ${year}`;
+		return `${dayNumber} of ${month}, ${year}`
 	}
 }
